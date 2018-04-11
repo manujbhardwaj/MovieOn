345846b2cf3826284f8c69d95b3b1174b26f92f1
kumaradi <kumar.adi@husky.neu.edu>
2018-04-11 13:42:00 -0400

    Updated user fav frontend

--- PhaseB/MovieOn/src/main/webapp/controller/user-fav.controller.client.js
+++ PhaseB/MovieOn/src/main/webapp/controller/user-fav.controller.client.js
@@ -1,12 +1,12 @@
 (function () {
     angular
-        .module("WebAppMaker")
+        .module("MovieOn")
         .controller("userFavController", userFavController);
-    function userFavController(userService, movieService, $location, checkLoggedIn) {
+    function userFavController(userService, movieService, favSellerService, $location, currentUser) {
         var vm = this;
-        if(checkLoggedIn){
-            vm.userId = checkLoggedIn.id;
-            vm.user = checkLoggedIn;
+        if(currentUser){
+            vm.userId = currentUser.id;
+            vm.user = currentUser;
         }
 
         /*event handlers*/
@@ -17,35 +17,10 @@
         vm.unfollowSeller = unfollowSeller;
         vm.contactAdmin = contactAdmin;
 
-        function unfollowSeller(_id, index) {
-            UserService
-                .unfollowSeller(userId, _id)
-                .then(function (res) {
-                    vm.sellerList[index].fav = false;
-                });
-        }
-
-        function contactAdmin() {
-            $location.url("/contact");
-        }
 
         function init() {
-            userService
-                .findUserById(userId)
-                .then(function (user) {
-                    vm.user = user;
-                    if(user.sellersFollowed.length == 0)
-                        vm.message = "You have no favorite sellers.";
-                    else{
-                        for(var i = 0; i < user.sellersFollowed.length; i++){
-                            user.sellersFollowed[i].fav = true;
-                        }
-                        vm.sellerList = user.sellersFollowed;
-                    }
-                });
-
+            getFavSeller();
             openNav();
-
             $(window).width(function() {
                 if ($(this).width() <= 768) {
                     closeNav();
@@ -57,8 +32,34 @@
         }
         init();
 
+        function getFavSeller() {
+            favSellerService
+                .getFavSeller(vm.userId)
+                .then(function (value) {
+                    console.log(value.data);
+                    if(value.data.length === 0){
+                        vm.message = "You have no favorite sellers.";
+                    }
+                    vm.sellerList = value.data;
+                }, function (reason) {
+                    vm.error = "Failed to retrieve favourite sellers."
+                });
+        }
+
+        function unfollowSeller(sellerId) {
+            favSellerService
+                .unfavSeller(vm.userId, sellerId)
+                .then(function (res) {
+                    getFavSeller();
+                });
+        }
+
+        function contactAdmin() {
+            $location.url("/contact");
+        }
+
         function logout() {
-            UserService
+            userService
                 .logout()
                 .then(function(response) {
                     $location.url("/home");
